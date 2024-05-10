7f77deb80feeef39097fef2e4426f55db3ef2157
NadiiaChernokur <chiernokur5@gmail.com>
2024-05-10 22:27:32 +0200

    dom

--- src/index.js
+++ src/index.js
@@ -1,4 +1,30 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import "./index.css";
+// import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+// import { Provider } from "react-redux";
+// import { BrowserRouter } from "react-router-dom";
+// import { PersistGate } from "redux-persist/integration/react";
+// import { persistor, store } from "./redux/store";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <PersistGate loading={null} persistor={persistor}>
+//         <BrowserRouter basename="/">
+//           <App />
+//         </BrowserRouter>
+//       </PersistGate>
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+// reportWebVitals();
+
 import React from "react";
+import { createRoot } from "react-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -8,8 +34,7 @@ import { BrowserRouter } from "react-router-dom";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
